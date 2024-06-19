44d55f61ee89269533e766eca82c7bf50a72a3bc
Fernando Bonfim <fernando.j.bonfim@gmail.com>
2024-06-19 19:48:14 -0300

    Initial files

---
+++ src/tipodevariaveldemo5/TipoDeVariavelDemo5.java
@@ -0,0 +1,12 @@
+package tipodevariaveldemo5;
+
+public class TipoDeVariavelDemo5 {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b é " + b);
+        b = true;
+        System.out.println("b é " + b);
+    }
+
+}
